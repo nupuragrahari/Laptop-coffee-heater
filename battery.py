9de484a837f76e579e58b188e53b30ebb4743c73
ashu94 <singhjug1994@gmail.com>
2015-10-27 19:17:38 +0530

    voltage updated

--- battery.py
+++ battery.py
@@ -2,3 +2,6 @@ import subprocess
 p = subprocess.Popen(['cat', '/sys/class/power_supply/BAT1/current_now'], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 out,err=p.communicate()
 print out
+p1 = subprocess.Popen(['cat', '/sys/class/power_supply/BAT1/voltage_now'], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+out,err=p1.communicate()
+print out
