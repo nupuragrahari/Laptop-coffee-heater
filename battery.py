f041d2ef6c12b5462f6bb478e284d91ed845584f
ashu94 <singhjug1994@gmail.com>
2015-10-28 21:57:32 +0530

    present value

--- battery.py
+++ battery.py
@@ -5,3 +5,6 @@ print out
 p1 = subprocess.Popen(['cat', '/sys/class/power_supply/BAT1/voltage_now'], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 out,err=p1.communicate()
 print out
+p2 = subprocess.Popen(['cat', '/sys/class/power_supply/BAT1/present'], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+out,err=p2.communicate()
+print out
